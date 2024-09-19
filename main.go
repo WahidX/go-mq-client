8474dbfde244c3bb0a276527524cc137eedf835b
Wahidx <wahidx93@gmail.com>
2024-09-19 14:04:05 +0530

    client working

--- main.go
+++ main.go
@@ -1,7 +1,86 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+var port = "4000"
 
 func main() {
-	fmt.Println("X")
+	// Connect to the server
+	conn, err := net.Dial("tcp", "localhost:"+port)
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	// Reader for user input
+	reader := bufio.NewReader(os.Stdin)
+
+	closeConn := func(err error) {
+		conn.Close()
+		log.Fatalf("Failed to send CONSUME command: %v", err)
+	}
+
+	for {
+		fmt.Print("Enter command (PUBLISH or CONSUME) (p/c): ")
+		input, _ := reader.ReadString('\n')
+
+		switch input[:len(input)-1] {
+		case "p":
+			fallthrough
+		case "PUBLISH":
+			fmt.Print("Enter binary message to send: ")
+			msg, _ := reader.ReadString('\n')
+			msg = strings.TrimSpace(msg)
+
+			// starting to send the inputs to the MQ server
+			// PUBLISH command (1 byte)
+			if _, err := conn.Write([]byte{1}); err != nil {
+				closeConn(err)
+			}
+
+			// Send message length (4 bytes)
+			if err := binary.Write(conn, binary.BigEndian, uint32(len(msg))); err != nil {
+				closeConn(err)
+			}
+
+			// Send the message
+			if _, err := conn.Write([]byte(msg)); err != nil {
+				closeConn(err)
+			}
+
+			// Read the server's acknowledgment
+			var response []byte
+			_, _ = bufio.NewReader(conn).Read(response)
+			fmt.Printf("Server Response: %s\n", response)
+
+		case "c":
+			fallthrough
+		case "CONSUME":
+			// Send CONSUME command (1 byte)
+			if _, err := conn.Write([]byte{2}); err != nil {
+				closeConn(err)
+			}
+
+			fmt.Print("Enter topic to consume: ")
+			topic, _ := reader.ReadString('\n')
+
+			if _, err = conn.Write([]byte(topic)); err != nil {
+				closeConn(err)
+			}
+
+			// Read the length of the binary message (4 bytes)
+			// Read the binary message
+
+		default:
+			fmt.Println("Unknown command")
+		}
+	}
 }
